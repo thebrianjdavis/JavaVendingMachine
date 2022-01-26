4f18e27c8dc23afcd559b9bac7acee80057f1fdc
Brian Davis <thebrianjdavis@gmail.com>
2022-01-26 12:17:00 -0500

    beginning IoC and injecting dependencies as configuration from Main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Read inventory in from CSV - FileReadDAO
-        // Create vendingItem objects - FileReadDAO
-        // Sort the HashMap by keys - FileReadDAO
-
-        // Display user menu - cli
-        // Print item inventory to terminal - cli
-        // Input money - cli
-        // Purchase item/remove from inventory/vend message - cli/FileWriteDAO
         // Write session to sales report - cli (hidden function)/FileWriteDAO
         // Reset Inventory - cli (hidden function)/FileWriteDAO
 
-        iFileReaderDAO fileReaderDAO = new FileReaderDAO("inventory.csv");
+        // Configuration to set dependencies:
+        String fileName = "inventory.csv";
+        iFileReaderDAO fileReaderDAO = new FileReaderDAO(fileName);
+//        String connectionString = null;
+//        iFileReaderDAO fileReaderDAO = new DatabaseDAO(connectionString);
+
+        // Creates and runs vending machine:
         VendingMachine vm = new VendingMachine((FileReaderDAO) fileReaderDAO);
         vm.runMachine();
     }
